9327b6166d48256ec1256103e1ea985b6e505e81
MoKhalil2000 <ps206867@summacollege.nl>
2025-05-13 09:17:58 +0200

    Fix test: homepage toont taken correct

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $fillable = ['title', 'completed'];
+    
+    public function test_homepage_laadt()
+{
+    // 1. Maak testdata aan
+    Task::create(['title' => 'Test Taak', 'completed' => false]);
+
+    // 2. Laad de pagina
+    $response = $this->get('/');
+
+    // 3. Controleer of het werkt
+    $response->assertStatus(200);
+    $response->assertSee('Test Taak');
 }
+}
+
